79500562068bebc6d3dc55f9b5965d4892b03f6a
“Abraham <mrabblessedslyst4christ099@gmail.com>
2020-07-04 19:53:32 +0300

    initial project commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<bod>
+        <header>
+            <na>
+                <div class="main-wrapper">
+                    <u>
+                        <li><a href="index.php">Home</a></li>
+                    </u>
+                </div>
+
+                <div class="nav-login">
+                    <form action="" METHOD="post">
+                        <input type="text" name="uid" placeholder="Username/e-mail address">
+                        <input type="password" name="pwd" placeholder="Password">
+                        <button type="submit" name="submit">Login</button>
+                    </form>
+                    <a href="signup.php">Sign up</a>
+                </div>
+            </na>
+        </header>
+
+    <section>
+
+    </section>
+
+</bod>
+</html>
